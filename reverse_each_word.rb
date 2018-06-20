099b5736d11bb091143aa8bf1ae1de61c0687688
Sharon Mun <sharonymun@gmail.com>
2018-06-20 13:45:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 
 def reverse_each_word(string)
    array = string.split(" ")
   new_array = array.collect{|a| a.reverse}
   new_array.join(" ")
+=======
+sentence1 = 
+def reverse_each_word(sentence1)
+  
+  sentence1.each do |words|
+    puts "#{words}"
+  end
+>>>>>>> 7d8d1c83ebcc7d616485119063354a8da71ebb79
 end
